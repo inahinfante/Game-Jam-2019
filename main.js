9cccf9defa0ef454bd5f99b51c0613d325d20cea
Inah Infante <ianinfan@edu.uwaterloo.ca>
2019-01-26 13:44:29 -0500

    Added counters, plants, cashiers
    Added and placed counters, plants, cashiers

--- main.js
+++ main.js
@@ -15,14 +15,29 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image("repeating-background", "assets/floor_tile.png");
+    this.load.image("cashier", "assets/cashier.png");
+    this.load.image("customer", "assets/customer.png");
+    this.load.image("floor_tile", "assets/floor_tile.png");
+    this.load.image("hcounter", "assets/hcounter.png");
+    this.load.image("mcounter", "assets/mcounter.png");
+    this.load.image("plant", "assets/plant.png");
+    this.load.image("lvcounter", "assets/lvcounter.png");
+    this.load.image("rvcounter", "assets/rvcounter.png");
 }
 
 function create ()
 {
     const {width, height} = this.sys.game.config;
-    const bg = this.add.tileSprite(0, 0, width, height, "repeating-background")
+    const bg = this.add.tileSprite(0, 0, width, height, "floor_tile")
     bg.setOrigin(0, 0)
+    this.add.image(400, 75, "hcounter");
+    this.add.image(75, 300, "lvcounter");
+    this.add.image(725, 275, "rvcounter");
+    this.add.image(400, 387, "mcounter");
+    this.add.image(75, 75, "plant");
+    this.add.image(725, 75, "plant");
+    this.add.image(75, 525, "cashier");
+    this.add.image(400, 525, "cashier");
 }
 
 function update ()
