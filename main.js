4c84c0381e8d6b97083a04d2351db634e6c4e0e3
mosiman <dillonchan97@gmail.com>
2019-01-26 10:48:30 -0500

    Some assets

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: "#222222",
+    parent:"game-continer",
     scene: {
         preload: preload,
         create: create,
@@ -13,10 +15,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image("repeating-background", "assets/floor_tile.png");
 }
 
 function create ()
 {
+    const {width, height} = this.sys.game.config;
+    const bg = this.add.tileSprite(0, 0, width, height, "repeating-background")
+    bg.setOrigin(0, 0)
 }
 
 function update ()
